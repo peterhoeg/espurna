9ea874bf350349258b353e531ece7c4f811a111e
Peter Hoeg <peter@hoeg.com>
2019-07-06 22:20:04 +0800

    homie

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c++-mode
+  (flycheck-gcc-language-standard . c++11))
+ (c-mode
+  (mode . c++)))
---
+++ code/espurna/homie.h
@@ -0,0 +1,20 @@
+#if HOMIE_SUPPORT
+
+#pragma once
+
+class BaseHomie {
+
+};
+
+class HomieDevice: public BaseHomie {
+};
+
+class HomieNode: public BaseHomie {
+
+}:
+
+class HomieProperty: public BaseHomie {
+
+};
+
+#endif // HOMIE_SUPPORT
